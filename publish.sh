fc2058122d3a0bae410466660d5f13ee713452fc
Oliver Münzberg <oliver.muenzberg@gmail.com>
2023-02-28 10:51:51 +0100

    mod publish

--- publish.sh
+++ publish.sh
@@ -4,9 +4,10 @@ rm -rf ./pub
 
 dotnet --version && \
 dotnet restore && \
-dotnet build --no-restore && \
-dotnet test --no-build && \
+dotnet build -c Release --no-restore && \
+dotnet test -c Release --no-build && \
 dotnet publish ./Spotisharp.Client/Spotisharp.Client.csproj -c Release -o ./pub/osx-x64 -r osx-x64 --no-self-contained -p:PublishSingleFile=true /p:DebugType=None /p:DebugSymbols=false && \
+dotnet publish ./Spotisharp.Client/Spotisharp.Client.csproj -c Release -o ./pub/osx-arm64 -r osx-arm64 --no-self-contained -p:PublishSingleFile=true /p:DebugType=None /p:DebugSymbols=false && \
 dotnet publish ./Spotisharp.Client/Spotisharp.Client.csproj -c Release -o ./pub/win-x64 -r win-x64 --no-self-contained -p:PublishSingleFile=true /p:DebugType=None /p:DebugSymbols=false && \
 dotnet publish ./Spotisharp.Client/Spotisharp.Client.csproj -c Release -o ./pub/win-x86 -r win-x86 --no-self-contained -p:PublishSingleFile=true /p:DebugType=None /p:DebugSymbols=false && \
 dotnet publish ./Spotisharp.Client/Spotisharp.Client.csproj -c Release -o ./pub/linux-x64 -r linux-x64 --no-self-contained -p:PublishSingleFile=true /p:DebugType=None /p:DebugSymbols=false && \
